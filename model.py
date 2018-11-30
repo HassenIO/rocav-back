9103773e4c973ce1f87c0139c0bc7d98edc84639
GHERBI Amine <amine.gherbi@axa.fr>
2018-11-30 17:26:55 +0000

    Updated model.py

--- model.py
+++ model.py
@@ -11,11 +11,14 @@ def model(compTerit,specialite,domaine):
     domaine='DomainLW_'+domaine
     if (compTerit in compTerritPossibles)&(specialite in specialitePossibles)&(domaine in domainesPossibles):
         dataPossibles= dataFinal[(dataFinal[compTerit]>0)&(dataFinal[specialite]>0)]
-        dataPossibles['Taux_missions_Accomplies']=dataPossibles['nb_mission']/max(dataPossibles['nb_mission']) 
-        criteres=dataPossibles['Taux de reussite']+dataPossibles['Taux Part reussite']-dataPossibles['Taux_Echec']+dataPossibles['Taux_missions_Accomplies']
-        dataPossibles['Critères']=(criteres)/float(max(criteres))
-        dataPossibles['Critères']=(criteres)/float(max(criteres))
-        dataSorted=dataPossibles.sort_values(by=['Taux_missions_Accomplies'],ascending=False)
         domaineColonne='nb_missions_'+domaine.replace("DomainLW_", "")
-        dataSorted[domaineColonne]=dataPossibles[domaine].astype(int)
-        return dataSorted[['AvocatId','Critères',domaineColonne]]
+        if (len(dataPossibles)!=0):
+            dataPossibles['Taux_missions_Accomplies']=dataPossibles['nb_mission']/max(dataPossibles['nb_mission']) 
+            criteres=dataPossibles['Taux de reussite']+dataPossibles['Taux Part reussite']-dataPossibles['Taux_Echec']+dataPossibles['Taux_missions_Accomplies']
+            dataPossibles['Critères']=(criteres)/float(max(criteres))
+            dataPossibles['Critères']=(criteres)/float(max(criteres))
+            dataSorted=dataPossibles.sort_values(by=['Taux_missions_Accomplies'],ascending=False)     
+            dataSorted[domaineColonne]=dataPossibles[domaine].astype(int)
+            return dataSorted[['AvocatId','Critères',domaineColonne]]
+        else:
+            return pd.DataFrame()
