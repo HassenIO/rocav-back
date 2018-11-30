299413b0e5bdf3634dd77f24573affb68e18f992
Hassen Taidirt <htaidirt@gmail.com>
2018-11-30 17:29:36 +0100

    Add reco function

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def model(compTerit,specialite,domaine):
     dataFinal = pd.read_csv("./dataFinal.csv", sep=",")
-    ###### listes des compétences,spécialités et domaines possibles
+    ###### listes des compÃ©tences,spÃ©cialitÃ©s et domaines possibles
     compTerritPossibles=[x for x in dataFinal.columns if ('CT_LWord_' in x)]
     specialitePossibles=[x for x in dataFinal.columns if ('SpecLWord_' in x)]
     domainesPossibles=[x for x in dataFinal.columns if ('DomainLW_' in x)]
@@ -13,9 +13,9 @@ def model(compTerit,specialite,domaine):
         dataPossibles= dataFinal[(dataFinal[compTerit]>0)&(dataFinal[specialite]>0)]
         dataPossibles['Taux_missions_Accomplies']=dataPossibles['nb_mission']/max(dataPossibles['nb_mission']) 
         criteres=dataPossibles['Taux de reussite']+dataPossibles['Taux Part reussite']-dataPossibles['Taux_Echec']+dataPossibles['Taux_missions_Accomplies']
-        dataPossibles['Critères']=(criteres)/float(max(criteres))
-        dataPossibles['Critères']=(criteres)/float(max(criteres))
+        dataPossibles['CritÃ¨res']=(criteres)/float(max(criteres))
+        dataPossibles['CritÃ¨res']=(criteres)/float(max(criteres))
         dataSorted=dataPossibles.sort_values(by=['Taux_missions_Accomplies'],ascending=False)
         domaineColonne='nb_missions_'+domaine.replace("DomainLW_", "")
         dataSorted[domaineColonne]=dataPossibles[domaine].astype(int)
-        return dataSorted[['AvocatId','Critères',domaineColonne]]
+        return dataSorted[['AvocatId','CritÃ¨res',domaineColonne]]
--- reco.py
+++ reco.py
@@ -1,26 +1,15 @@
 import json
 
-def run(event, context):
-    body = {
-        "message": "Serverless get inputs for front",
-        "input": event
-    }
+from model import model
 
+def run(event, context):
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "result": model(event["compTerit"], event["specialite"], event["domaine"]).to_json(orient='records')
     }
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
 
 if __name__ == "__main__":
     run("", "")
