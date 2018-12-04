f00454fbd16a806f5dddd7f09c0acd84a98b5104
GHERBI Amine <amine.gherbi@axa.fr>
2018-12-04 13:35:50 +0000

    Updated model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def model(compTerit,specialite,domaine):
     dataFinal = pd.read_csv("./dataFinal.csv", sep=",")
-     ###### listes des compétences,spécialités et domaines possibles
+    ###### listes des compétences,spécialités et domaines possibles
     compTerritPossibles=[x for x in dataFinal.columns if ('CT_LWord_' in x)]
     specialitePossibles=[x for x in dataFinal.columns if ('SpecLWord_' in x)]
     domainesPossibles=[x for x in dataFinal.columns if ('DomainLW_' in x)]
@@ -12,17 +12,22 @@ def model(compTerit,specialite,domaine):
     if (compTerit in compTerritPossibles)&(specialite in specialitePossibles)&(domaine in domainesPossibles):
         dataPossibles= dataFinal[(dataFinal[compTerit]>0)&(dataFinal[specialite]>0)]
         domaineColonne='nb_missions_'+domaine.replace("DomainLW_", "")
-        dataPossibles['Taux_missions_Accomplies']=dataPossibles['nb_mission']/max(dataPossibles['nb_mission'])
-        dataPossibles['Taux_missions_Domaine_Accomplies']=dataPossibles[domaine]/sum(dataPossibles[domaine])
-        dataPossibles[domaineColonne]=dataPossibles[domaine].astype(int)
-        criteres=dataPossibles['Taux de reussite']+dataPossibles['Taux Part reussite']-dataPossibles['Taux_Echec']+dataPossibles['Taux_missions_Accomplies']+dataPossibles['Taux_missions_Domaine_Accomplies']
-        if (len(dataPossibles)>1):         
-            dataPossibles['Critères']=(criteres-min(criteres))/float(max(criteres)-min(criteres))
-            dataSorted=dataPossibles.sort_values(by=['Critères'],ascending=False)
-            return dataSorted[['AvocatId','nameAvocat','Critères',domaineColonne,'Partenaire', 'Côut']]
-        elif (len(dataPossibles)==1):
-            dataPossibles['Critères']=1
-            return dataPossibles[['AvocatId','nameAvocat','Critères',domaineColonne,'Partenaire', 'Côut']]
+        if (len(dataPossibles)>=1):
+            dataPossibles['Taux_missions_Accomplies']=dataPossibles['nb_mission']/max(dataPossibles['nb_mission'])
+            if (sum(dataPossibles[domaine])!=0):
+                dataPossibles['Taux_missions_Domaine_Accomplies']=dataPossibles[domaine]/sum(dataPossibles[domaine])
+            else:
+                dataPossibles['Taux_missions_Domaine_Accomplies']=0
+            dataPossibles[domaineColonne]=dataPossibles[domaine].astype(int)
+            criteres=dataPossibles['Taux de reussite']+dataPossibles['Taux Part reussite']-dataPossibles['Taux_Echec']+dataPossibles['Taux_missions_Accomplies']+dataPossibles['Taux_missions_Domaine_Accomplies']
+            if (max(criteres)!=min(criteres)):
+                dataPossibles['Critères']=(criteres-min(criteres))/float(max(criteres)-min(criteres))
+                dataSorted=dataPossibles.sort_values(by=['Critères'],ascending=False)
+                return dataSorted[['AvocatId','nameAvocat','Critères',domaineColonne,'Partenaire', 'Côut']]
+            else:
+                dataPossibles['Critères']=1
+                return dataPossibles[['AvocatId','nameAvocat','Critères',domaineColonne,'Partenaire', 'Côut']]
         else:
             return pd.DataFrame()
+
 
